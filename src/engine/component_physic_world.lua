bcf3fe94efdcb1ade1b7fe0a2315b9f2c8bf958a
gogoprog <gogoprog@gmail.com>
2013-02-08 09:39:01 +0100

    Added fixed rate for physic update

--- src/engine/component_physic_world.lua
+++ src/engine/component_physic_world.lua
@@ -1,9 +1,12 @@
 require 'engine.class'
 
+local FIXED_RATE = 0.01
+
 COMPONENT_PHYSIC_WORLD = class(function(o,parameters,entity)
     local gravity = parameters.Gravity or { 0, 9.81 * 64 }
     o.World = love.physics.newWorld(gravity[1], gravity[2], true)
     o.World:setCallbacks(COMPONENT_PHYSIC_WORLD.CollisionStart)
+    o.TimeSum = 0
     o.Entity = entity
     o.Entity.World = o.World
 end)
@@ -22,7 +25,12 @@ end
 -- METHODS
 
 function COMPONENT_PHYSIC_WORLD:Update(dt)
-    self.World:update(dt)
+    self.TimeSum = self.TimeSum + dt
+
+    while self.TimeSum >= FIXED_RATE do
+        self.World:update(FIXED_RATE)
+        self.TimeSum = self.TimeSum - FIXED_RATE
+    end
 end
 
 function COMPONENT_PHYSIC_WORLD:Render()
