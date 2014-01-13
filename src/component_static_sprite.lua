e50fb4ad701908f297ca3f13d4f202fc5490f5b4
Gauthier Billot <gauthier.billot@fishingcactus.com>
2014-01-13 21:27:54 +0100

    Fixed static sprite position

--- src/component_static_sprite.lua
+++ src/component_static_sprite.lua
@@ -4,7 +4,16 @@ COMPONENT_STATIC_SPRITE = class(function(o,parameters,entity)
     o.Quad = parameters.Quad
     o.Entity = entity
     local p = entity.Position
-    COMPONENT_SPRITE_BATCH.Current:add(parameters.Quad,p[1],p[2])
+    local x,h,w,h = o.Quad:getViewport()
+
+    COMPONENT_SPRITE_BATCH.Current:add(
+        parameters.Quad,
+        p[1],p[2],
+        entity.Orientation,
+        1, 1,
+        w * 0.5,
+        h * 0.5
+        )
 end)
 
 -- METHODS
