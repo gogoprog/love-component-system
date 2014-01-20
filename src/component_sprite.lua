ee42553d4d4b68f6e36faf717125bf5e11e37960
Gauthier Billot <gauthier.billot@fishingcactus.com>
2014-01-20 19:00:45 +0100

    Updated component sprite

--- src/component_sprite.lua
+++ src/component_sprite.lua
@@ -2,14 +2,26 @@ require 'lcs.class'
 
 COMPONENT_SPRITE = class(function(o,parameters,entity)
     o.Texture = parameters.Texture
+    o.Quad = parameters.Quad
     o.Extent = parameters.Extent
     o.Offset = parameters.Offset
     o.Layer = parameters.Layer or 1
     o.Entity = entity
-    o.ScaleFactorX = o.Extent[1] / o.Texture:getWidth()
-    o.ScaleFactorY = o.Extent[2] / o.Texture:getHeight()
-    o.OffsetX = o.Texture:getWidth() * 0.5
-    o.OffsetY = o.Texture:getHeight() * 0.5
+
+    local x,y,w,h
+
+    if o.Quad then
+        x,y,w,h = o.Quad:getViewport()
+    else
+        w = o.Texture:getWidth()
+        h = o.Texture:getHeight()
+    end
+
+    o.ScaleFactorX = o.Extent[1] / w
+    o.ScaleFactorY = o.Extent[2] / h
+    o.OffsetX = w * 0.5
+    o.OffsetY = h * 0.5
+
 end)
 
 -- METHODS
