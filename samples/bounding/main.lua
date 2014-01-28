0c1213089c935e44d4b7ed6cdccdc927861ba454
Gauthier Billot <gauthier.billot@fishingcactus.com>
2014-01-28 20:33:59 +0100

    Updated bounding sample

--- samples/bounding/main.lua
+++ samples/bounding/main.lua
@@ -38,13 +38,15 @@ function love.load(arg)
     userblock = ENTITY(descriptions.Block,{400,300})
 end
 
+local x1,y1,x2,y2 = 20,250,300,40
+
 function love.update(dt)
     ENGINE.Update(dt)
 
     local x,y = love.mouse.getPosition()
     userblock.Position = {x,y}
 
-    if userblock:CollidesWithWorld() then
+    if userblock:CollidesWithWorld() or userblock:Intersects(x1,y1,x2,y2) then
         userblock:SetQuadColor({255,0,0,255})
     else
         userblock:SetQuadColor({255,255,255,255})
@@ -52,6 +54,8 @@ function love.update(dt)
 end
 
 function love.draw()
+    love.graphics.setColor(255,255,255,255)
+    love.graphics.line(x1,y1,x2,y2)
     ENGINE.Render()
 end
 
